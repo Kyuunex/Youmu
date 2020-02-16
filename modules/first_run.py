227acb301709f9ed6c20b166759c4ed4ed1909ad
Kyuunex <kyuunex@protonmail.ch>
2020-02-17 01:17:02 +0400

    migrate first_run.py to aiosqlite

--- modules/first_run.py
+++ modules/first_run.py
@@ -1,36 +1,44 @@
-from modules import db
+import sqlite3
 from modules.connections import database_file as database_file
 import os
 
 
 async def add_admins(self):
-    if not db.query("SELECT * FROM admins"):
+    async with await self.db.execute("SELECT * FROM admins") as cursor:
+        admin_list = await cursor.fetchall()
+
+    if not admin_list:
         app_info = await self.application_info()
         if app_info.team:
             for team_member in app_info.team.members:
-                db.query(["INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"]])
+                await self.db.execute("INSERT INTO admins VALUES (?, ?)", [str(team_member.id), "1"])
                 print(f"Added {team_member.name} to admin list")
         else:
-            db.query(["INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"]])
+            await self.db.execute("INSERT INTO admins VALUES (?, ?)", [str(app_info.owner.id), "1"])
             print(f"Added {app_info.owner.name} to admin list")
+        await self.db.commit()
 
 
 def create_tables():
     if not os.path.exists(database_file):
-        db.query("CREATE TABLE config (setting, parent, value, flag)")
-        db.query("CREATE TABLE admins (user_id, permissions)")
+        conn = sqlite3.connect(database_file)
+        c = conn.cursor()
+        c.execute("CREATE TABLE config (setting, parent, value, flag)")
+        c.execute("CREATE TABLE admins (user_id, permissions)")
 
-        db.query("CREATE TABLE rssfeed_tracklist (url)")
-        db.query("CREATE TABLE rssfeed_channels (url, channel_id)")
-        db.query("CREATE TABLE rssfeed_history (url, entry_id)")
+        c.execute("CREATE TABLE rssfeed_tracklist (url)")
+        c.execute("CREATE TABLE rssfeed_channels (url, channel_id)")
+        c.execute("CREATE TABLE rssfeed_history (url, entry_id)")
 
-        db.query("CREATE TABLE rankfeed_channel_list (channel_id)")
-        db.query("CREATE TABLE rankfeed_history (mapset_id)")
+        c.execute("CREATE TABLE rankfeed_channel_list (channel_id)")
+        c.execute("CREATE TABLE rankfeed_history (mapset_id)")
 
-        db.query("CREATE TABLE usereventfeed_tracklist (osu_id)")
-        db.query("CREATE TABLE usereventfeed_channels (osu_id, channel_id)")
-        db.query("CREATE TABLE usereventfeed_history (osu_id, event_id, timestamp)")
+        c.execute("CREATE TABLE usereventfeed_tracklist (osu_id)")
+        c.execute("CREATE TABLE usereventfeed_channels (osu_id, channel_id)")
+        c.execute("CREATE TABLE usereventfeed_history (osu_id, event_id, timestamp)")
 
-        db.query("CREATE TABLE groupfeed_channel_list (channel_id)")
-        db.query("CREATE TABLE groupfeed_members (osu_id, username, group_id)")
-        db.query("CREATE TABLE groupfeed_json_data (group_id, contents)")
+        c.execute("CREATE TABLE groupfeed_channel_list (channel_id)")
+        c.execute("CREATE TABLE groupfeed_members (osu_id, username, group_id)")
+        c.execute("CREATE TABLE groupfeed_json_data (group_id, contents)")
+        conn.commit()
+        conn.close()
