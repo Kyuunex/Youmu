729cc19e2fc1b69d854ae11b60ff5d26f2fb054f
Kyuunex <kyuunex@protonmail.ch>
2020-02-26 16:42:05 +0400

    added unnest function in wrappers.py

--- modules/wrappers.py
+++ modules/wrappers.py
@@ -31,6 +31,13 @@ def in_db_list(a_list, what):
     return False
 
 
+def unnest_list(a_list):
+    buffer_list = []
+    for item in a_list:
+        buffer_list.append(item[0])
+    return buffer_list
+
+
 def get_member_guaranteed(ctx, lookup):
     if len(ctx.message.mentions) > 0:
         return ctx.message.mentions[0]
