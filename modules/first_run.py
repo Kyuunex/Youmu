7bde7cea02dd4d572d1a08c232d0042b04b30991
Kyuunex <kyuunex@protonmail.ch>
2020-10-31 19:30:26 +0400

    ensure tables

--- modules/first_run.py
+++ modules/first_run.py
@@ -19,91 +19,90 @@ async def add_admins(self):
         await self.db.commit()
 
 
-def create_tables():
-    if not os.path.exists(database_file):
-        conn = sqlite3.connect(database_file)
-        c = conn.cursor()
-        c.execute("""
-        CREATE TABLE "admins" (
-            "user_id"    INTEGER NOT NULL UNIQUE,
-            "permissions"    INTEGER NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "config" (
-            "setting"    TEXT,
-            "parent"    TEXT,
-            "value"    TEXT,
-            "flag"    TEXT
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "groupfeed_channel_list" (
-            "channel_id"    INTEGER NOT NULL UNIQUE
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "groupfeed_group_members" (
-            "osu_id"    INTEGER NOT NULL,
-            "group_id"    INTEGER NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "groupfeed_member_info" (
-            "osu_id"    INTEGER NOT NULL UNIQUE,
-            "username"    TEXT NOT NULL,
-            "country"    TEXT NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "ignored_users" (
-            "user_id"    INTEGER NOT NULL UNIQUE,
-            "reason"    TEXT NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "rankfeed_channel_list" (
-            "channel_id"    INTEGER NOT NULL UNIQUE
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "rankfeed_history" (
-            "mapset_id"    INTEGER NOT NULL UNIQUE
-        )
-        """)
-        c.execute("""CREATE TABLE "rssfeed_channels" (
-            "url"    TEXT NOT NULL,
-            "channel_id"    INTEGER NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "rssfeed_history" (
-            "url"    TEXT NOT NULL,
-            "entry_id"    TEXT NOT NULL UNIQUE
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "rssfeed_tracklist" (
-            "url"    TEXT NOT NULL UNIQUE
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "usereventfeed_channels" (
-            "osu_id"    INTEGER NOT NULL,
-            "channel_id"    INTEGER NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "usereventfeed_history" (
-            "osu_id"    INTEGER NOT NULL,
-            "event_id"    INTEGER NOT NULL UNIQUE,
-            "timestamp"    INTEGER NOT NULL
-        )
-        """)
-        c.execute("""
-        CREATE TABLE "usereventfeed_tracklist" (
-            "osu_id"    INTEGER NOT NULL UNIQUE
-        )
-        """)
-        conn.commit()
-        conn.close()
+def ensure_tables():
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "admins" (
+        "user_id"    INTEGER NOT NULL UNIQUE,
+        "permissions"    INTEGER NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "config" (
+        "setting"    TEXT,
+        "parent"    TEXT,
+        "value"    TEXT,
+        "flag"    TEXT
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "groupfeed_channel_list" (
+        "channel_id"    INTEGER NOT NULL UNIQUE
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "groupfeed_group_members" (
+        "osu_id"    INTEGER NOT NULL,
+        "group_id"    INTEGER NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "groupfeed_member_info" (
+        "osu_id"    INTEGER NOT NULL UNIQUE,
+        "username"    TEXT NOT NULL,
+        "country"    TEXT NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "ignored_users" (
+        "user_id"    INTEGER NOT NULL UNIQUE,
+        "reason"    TEXT NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "rankfeed_channel_list" (
+        "channel_id"    INTEGER NOT NULL UNIQUE
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "rankfeed_history" (
+        "mapset_id"    INTEGER NOT NULL UNIQUE
+    )
+    """)
+    c.execute("""CREATE TABLE IF NOT EXISTS "rssfeed_channels" (
+        "url"    TEXT NOT NULL,
+        "channel_id"    INTEGER NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "rssfeed_history" (
+        "url"    TEXT NOT NULL,
+        "entry_id"    TEXT NOT NULL UNIQUE
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "rssfeed_tracklist" (
+        "url"    TEXT NOT NULL UNIQUE
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "usereventfeed_channels" (
+        "osu_id"    INTEGER NOT NULL,
+        "channel_id"    INTEGER NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "usereventfeed_history" (
+        "osu_id"    INTEGER NOT NULL,
+        "event_id"    INTEGER NOT NULL UNIQUE,
+        "timestamp"    INTEGER NOT NULL
+    )
+    """)
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS "usereventfeed_tracklist" (
+        "osu_id"    INTEGER NOT NULL UNIQUE
+    )
+    """)
+    conn.commit()
+    conn.close()
